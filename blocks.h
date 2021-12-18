875521e9f5b4737f315395be1e81f22e8deee12f
Kishore Satheeskumar <k.sath214@gmail.com>
2021-12-18 22:16:56 +0000

    Initial commit

---
+++ blocks.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+	{"🕗 ", "date '+%b %d (%a) %I:%M%p' ",		5,		0},
+	{"📦 ", "~/.local/bin/pacupdate", 360, 9},
+	{"🔊 ", "~/.local/bin/volume", 2, 10},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
